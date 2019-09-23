4d161b5c647b2f216e8aa5c689c60c2b187a7db7
Rene Schouten <rene@twtg.io>
2019-09-23 11:16:47 +0200

    dllcreate made, working on dllAddBeforeHead()

--- doublyLinkedList.c
+++ doublyLinkedList.c
@@ -2,14 +2,42 @@
 
 DoublyLinkedList *dllCreate(void)
 {
-
+    DoublyLinkedList* newList = malloc(sizeof(newList));
+    if(newList == NULL) return NULL;
+    newList->head = NULL;
+    newList->tail = NULL;
+    return newList;
 }
 void dllDelete(DoublyLinkedList **ptrTheList)
 {
-
+    //todo: delete items
+    if(*ptrTheList)
+    {
+       // free(*ptrTheList);
+    }
+    *ptrTheList = NULL;
 }
 DllNode *dllAddBeforeHead(DoublyLinkedList *theList, void *theData)
 {
+    if(theList == NULL) return NULL;
+    if(theData == NULL) return NULL;
+    DllNode* newNode = malloc(sizeof(newNode));
+    if(newNode == NULL) return NULL;
+
+    newNode->data = theData;
+
+    newNode->previous = NULL;
+    if(theList->head)
+    {
+        newNode->next = theList->head;
+        newNode->next->previous = newNode;
+    }
+    else {
+        newNode->next = NULL;
+        theList->tail = newNode;
+    }
+    theList->head = newNode;
+    char* a = "a";
 
 }
 DllNode *dllAddAfterTail(DoublyLinkedList *theList, void *theData)
