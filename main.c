77b8da24e85fefc1e392109aeb3f8977dc6b5401
Raber <raberhawler@hotmail.com>
2019-10-28 11:37:34 +0100

    begin aan belford gemaakt

--- main.c
+++ main.c
@@ -2,15 +2,62 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
+#include <limits.h>
 #include "lib/munit/munit.h"
 #include "lib/graph/graph.h"
 #include "lib/cursor/graphcursor.h"
 
 #include "FileToGraph.h"
 
+
+void bellmanFordAlg(Graph* graphToSearch, Vertex* start, Vertex* destination){
+    if(graphToSearch == NULL) return;
+    if(start == NULL) return;
+    if(destination == NULL) return;
+
+    //assigning infinity to all other vertexes excluding start vertex
+    DllNode* vertexLoop = graphToSearch->vertices->head;
+    int* pathValues = malloc(sizeof(int));
+    while (vertexLoop)
+    {
+        if(start == vertexLoop){          
+            vertexLoop->data=0;
+       }
+       else if(destination == vertexLoop){
+           vertexLoop->data=NULL;
+       }
+        vertexLoop->data=INT_MAX;
+        vertexLoop=vertexLoop->next;
+    }
+
+    //relaxing the edges
+    vertexLoop = start;
+    int aantalVertices = numberOfVertexs(graphToSearch);
+    int teller = 0;
+    while(vertexLoop){
+
+        //do stuff with edge
+
+        if(vertexLoop= graphToSearch->vertices->tail && teller < aantalVertices-1){
+            vertexLoop=graphToSearch->vertices->head;
+        }
+        if(teller = aantalVertices){
+            break;
+        }
+
+        vertexLoop=vertexLoop->next;
+        teller= teller+1;
+    }
+
+
+
+
+}
+
 int main(int argc, char *argv[])
 {
     Graph* graph = loadGraphFromFile("json/citiesShortestPath.json");
+    
     if(graph == NULL) return 0;
 
     GraphCursor* cursor = createCursor();
